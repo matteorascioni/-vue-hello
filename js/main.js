507fc3a1b24c63e9f5723c4b8e29f515a5dfef71
Matteo <Rascioni>
2020-11-06 16:14:27 +0100

    Complete!

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-console.log('hello');
-
+// VUE JS 
 const app = new Vue({
     el: '#app',
     data: {
         title: 'Hello world',
-        imgApp: 'https://www.viaggi-brevi.com/wp-content/uploads/2018/02/caraibi-660x330.jpg'
+        imgApp: 'https://images.unsplash.com/photo-1547659919-2da4556aa183?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80'
     }
 });
