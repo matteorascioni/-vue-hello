79ec10ed65507d7dba4460e2d56f3f4088453476
Matteo <Rascioni>
2020-11-06 16:06:04 +0100

    Complete! 100%

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log('hello');
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Hello world',
+        imgApp: 'https://www.viaggi-brevi.com/wp-content/uploads/2018/02/caraibi-660x330.jpg'
+    }
+});
